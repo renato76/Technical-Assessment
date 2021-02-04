a6d9e5bf84c97047a7e67d839f104ebd2d4066f9
renato76 <renmignogna@gmail.com>
2021-02-04 16:20:04 +0000

    added some notes in readme

--- solution.js
+++ solution.js
@@ -1,20 +1,21 @@
 const accountTypeChecker = (accountBalanceHistory) => {
 
-  // first I create an empty array to which I will push the values of difference in balances from month to month
+  // First I create an empty array to which I will push the values of difference in balances from month to month
   const numbers = []
 
+  // Next step, find the balance amounts within nested array of objects
   const balances = accountBalanceHistory.map(val => val.account.balance.amount).reverse()
   // console.log(balances)
 
-  // create a for loop to check each value in the balances array and subtract it from the next value in the array
-  // then push that new value into the numbers array
+  // Next, create a for loop to check each value in the balances array and subtract it from the next value in the array
+  // Then push that new value into the numbers array
   for (let i = 0; i < balances.length - 1; i++) {
     numbers.push(balances[i] - balances[i + 1])
   }
 
   // console.log(numbers)
 
-  // This checks if all numbers (differences in balance) in the array are the same and returns true or false
+  // Finally, check if all numbers (differences in balance) in the array are the same and returns true or false
 
   const result = numbers.every(value => value === numbers[0])
 
