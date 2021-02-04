e2c54e2f8804dd7134a90eb54cba6fa491b603f8
renato76 <renmignogna@gmail.com>
2021-02-04 15:51:52 +0000

    installed jest and created tests - all tests passed

--- solution.js
+++ solution.js
@@ -4,7 +4,7 @@ const accountTypeChecker = (accountBalanceHistory) => {
   const numbers = []
 
   const balances = accountBalanceHistory.map(val => val.account.balance.amount).reverse()
-  console.log(balances)
+  // console.log(balances)
 
   // create a for loop to check each value in the balances array and subtract it from the next value in the array
   // then push that new value into the numbers array
@@ -12,34 +12,40 @@ const accountTypeChecker = (accountBalanceHistory) => {
     numbers.push(balances[i] - balances[i + 1])
   }
 
-  console.log(numbers)
+  // console.log(numbers)
 
   // This checks if all numbers (differences in balance) in the array are the same and returns true or false
 
   const result = numbers.every(value => value === numbers[0])
 
-  console.log(result)
+  // console.log(result)
 
-  console.log(!result ? 'A' : 'B')
+  return !result ? 'A' : 'B'
+}
+
+module.exports = {
+  accountTypeChecker
 }
 
 
-accountTypeChecker([{
-  monthNumber: 0, // current month
-  account: {
-    balance: { amount: 100 }
-  }
-},
-{
-  monthNumber: 1, // last month
-  account: {
-    balance: { amount: 200 }
-  }
-},
-{
-  monthNumber: 2, // two months ago
-  account: {
-    balance: { amount: 300 }
-  }
-}])
+// accountTypeChecker([{
+//   monthNumber: 0, // current month
+//   account: {
+//     balance: { amount: 100 }
+//   }
+// },
+// {
+//   monthNumber: 1, // last month
+//   account: {
+//     balance: { amount: 200 }
+//   }
+// },
+// {
+//   monthNumber: 2, // two months ago
+//   account: {
+//     balance: { amount: 300 }
+//   }
+// }])
+
+
 
---
+++ solution.test.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-undef */
+const fns = require('./solution')
+
+describe('accountTypeChecker', () => {
+  it('should return A if balance amount decreases by varying amounts each month or B if the balance decreases by same amount each month', () => {
+    // test with same decreasing values
+    expect(fns.accountTypeChecker([{
+      monthNumber: 0, // current month
+      account: {
+        balance: { amount: 100 }
+      }
+    },
+    {
+      monthNumber: 1, // last month
+      account: {
+        balance: { amount: 200 }
+      }
+    },
+    {
+      monthNumber: 2, // two months ago
+      account: {
+        balance: { amount: 300 }
+      }
+    }])).toEqual('B')
+    // test with varying decreasing values
+    expect(fns.accountTypeChecker([{
+      monthNumber: 0, // current month
+      account: {
+        balance: { amount: 50 }
+      }
+    },
+    {
+      monthNumber: 1, // last month
+      account: {
+        balance: { amount: 200 }
+      }
+    },
+    {
+      monthNumber: 2, // two months ago
+      account: {
+        balance: { amount: 300 }
+      }
+    }])).toEqual('A')
+    // test with fixed negative values
+    expect(fns.accountTypeChecker([{
+      monthNumber: 0, // current month
+      account: {
+        balance: { amount: -100 }
+      }
+    },
+    {
+      monthNumber: 1, // last month
+      account: {
+        balance: { amount: -200 }
+      }
+    },
+    {
+      monthNumber: 2, // two months ago
+      account: {
+        balance: { amount: -300 }
+      }
+    }])).toEqual('B')
+  })
+})
